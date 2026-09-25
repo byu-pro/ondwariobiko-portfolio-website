3ccf0b12bf820ca8e0794d80102f1ab53895b16e
John obiko <obi22004@byui.edu>
2026-09-25 22:25:14 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     base:
-      process.env.GITHUB_PAGES === "true"
+      process.env["GITHUB_PAGES"] === "true"
         ? "/ondwariobiko-portfolio-website/"
         : "/",
   },
